68e61aa008b6d73097e1241e975f84f71bcd999d
Bruna Caletti <c4l3tt1@gmail.com>
2024-05-27 22:59:07 -0300

    Insert tabs on HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-const HomePage = async () => {
+import { DynamicTabs } from '@/components/dynamicTabs'
+import { dynamicTabsList } from '@/data/tabs'
+
+const HomePage = () => {
+  const hasTabsList = dynamicTabsList && dynamicTabsList.length > 0
   return (
     <section className="">
-      <div className="container md:px-4">
-        <h1>Página inicial</h1>
-      </div>
+      <div className="container md:px-4">{hasTabsList && <DynamicTabs tabs={dynamicTabsList} />}</div>
     </section>
   )
 }
