ea897f9db3b745e0c3615295cf4ad6351c72a125
Bruna Caletti <c4l3tt1@gmail.com>
2024-06-04 03:27:32 -0300

    Adjust axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,11 +1,6 @@
 import axios from 'axios'
 
-export const API_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? process.env.NEXT_PUBLIC_LOCALHOST_URL
-    : process.env.NEXT_PUBLIC_PRODUCTION_URL
+export const BASE_URL = 'https://market-fruit-api.vercel.app'
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
