9722c687b434aec83b01210087d24df30db35189
Bruna Caletti <c4l3tt1@gmail.com>
2024-06-04 21:47:51 -0300

    List fruits and tab content adjusts

--- src/components/listFruits/listFruits.component.tsx
+++ src/components/listFruits/listFruits.component.tsx
@@ -1,19 +1,44 @@
-import { getFruits } from '@/api/services/fruits'
+'use client'
+import { deleteFruit, getFruits } from '@/api/services/fruits'
+import { IFruit } from '@/types/fruits'
+import { cn } from '@/utils/classes'
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { LoadingList } from '../loadingList'
+import { DeleteFruitButton } from './buttonDelete'
 
-export const ListFruits = async () => {
-  const listFruits = await getFruits()
+export const ListFruits = ({ listFruitsProps }: { listFruitsProps: IFruit[] }) => {
+  const [listFruits, setListFruits] = useState<IFruit[]>(listFruitsProps)
+  const [loading, setLoading] = useState<boolean>(true)
 
-  if (!listFruits || !listFruits.length) return <>Não há dados</>
+  const listFruitsFetch = async () => {
+    const listFruits = await getFruits()
+    setListFruits(listFruits)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    listFruitsFetch()
+  }, [])
+
+  const handleDeleteFruit = async (fruitId: string) => {
+    await deleteFruit(fruitId)
+    listFruitsFetch()
+  }
+
+  if (loading) {
+    return <LoadingList />
+  }
   return (
     <>
-      {listFruits && listFruits.length > 0 && (
+      {listFruits && listFruits.length > 0 ? (
         <TableContainer>
           <Table sx={{ minWidth: 650 }} aria-label="Table fruits" stickyHeader>
             <TableHead>
               <TableRow>
                 <TableCell>Name</TableCell>
-                <TableCell align="right">Price</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell align="right">Delete Fruit</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -22,12 +47,21 @@ export const ListFruits = async () => {
                   <TableCell component="th" scope="row">
                     {fruit?.name}
                   </TableCell>
-                  <TableCell align="right">R$ {fruit?.price?.toFixed(2)}</TableCell>
+                  <TableCell>R$ {fruit?.price}</TableCell>
+                  <TableCell align="right">
+                    <DeleteFruitButton
+                      onDeleteFruit={handleDeleteFruit}
+                      fruitId={fruit.id}
+                      disabled={fruit.isInsideBucket}
+                    />
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
+      ) : (
+        <div className={cn('flex flex-col items-center justify-center w-full min-h-20')}>No data</div>
       )}
     </>
   )
