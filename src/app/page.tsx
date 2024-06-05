8ea8f08a5126882711318cccbf906a11cc4ad3eb
Bruna Caletti <c4l3tt1@gmail.com>
2024-06-04 21:48:00 -0300

    Page structure adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,33 @@
+import { getBuckets } from '@/api/services/buckets'
+import { getFruits } from '@/api/services/fruits'
 import { DynamicTabs } from '@/components/dynamicTabs'
+import { LoadingList } from '@/components/loadingList'
 import { TabContentBucket } from '@/components/tabContentBucket'
 import { TabContentFruits } from '@/components/tabContentFruits'
+import { Suspense } from 'react'
 
-const HomePage = () => {
+const HomePage = async () => {
+  const listBuckets = await getBuckets()
+  const listFruits = await getFruits()
+
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+
   const dynamicTabsList = [
     {
       label: 'Fruits',
-      content: <TabContentFruits />,
+      content: (
+        <Suspense fallback={<LoadingList />}>
+          <TabContentFruits listFruits={listFruits} />
+        </Suspense>
+      ),
     },
     {
       label: 'Buckets',
-      content: <TabContentBucket />,
+      content: (
+        <Suspense fallback={<LoadingList />}>
+          <TabContentBucket listBuckets={listBuckets} listFruits={listFruits} />
+        </Suspense>
+      ),
     },
   ]
   return (
