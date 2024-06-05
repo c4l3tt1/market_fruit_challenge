406a49b1278ceb478c2dab07c8e70c90187000d0
Bruna Caletti <c4l3tt1@gmail.com>
2024-06-04 21:47:06 -0300

    Forms adjusts

--- src/components/bucketForm/bucketForm.component.tsx
+++ src/components/bucketForm/bucketForm.component.tsx
@@ -1,40 +1,81 @@
 'use client'
 import { IBucketFormProps } from '@/types/buckets'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
+import { Button, TextField } from '@mui/material'
+import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 
 const schema = yup.object().shape({
   name: yup.string().required('Nome é obrigatório'),
   capacity: yup
     .number()
-    .required('Capacidade é obrigatória')
-    .positive('Capacidade deve ser um número positivo')
-    .integer('Capacidade deve ser um número inteiro'),
+    .typeError('Only numbers are accepted')
+    .required('Capacity id required')
+    .positive('Must be a positive number')
+    .integer('Must be an integer'),
 })
 
 export const BucketForm = ({ onSubmit }: IBucketFormProps) => {
   const {
-    register,
     handleSubmit,
     formState: { errors },
+    control,
   } = useForm({
     resolver: yupResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      capacity: 1,
+    },
   })
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="name">Nome do Balde</label>
-        <input id="name" {...register('name')} />
-        {errors.name && <p>{errors.name.message}</p>}
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-80 flex flex-col gap-y-6">
+      <div className="w-full">
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              placeholder="Type a name"
+              label="Bucket name"
+              error={!!errors.name}
+              helperText={errors.name?.message}
+              variant="outlined"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          )}
+        />
       </div>
-      <div>
-        <label htmlFor="capacity">Capacidade do Balde</label>
-        <input id="capacity" type="number" {...register('capacity')} />
-        {errors.capacity && <p>{errors.capacity.message}</p>}
+      <div className="w-full">
+        <Controller
+          name="capacity"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              placeholder="Type a number"
+              label="Bucket capacity"
+              error={!!errors.capacity}
+              helperText={errors.capacity?.message}
+              variant="outlined"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          )}
+        />
       </div>
-      <button type="submit">Criar Balde</button>
+      <div className="flex w-full justify-end -mt-3">
+        <Button type="submit" variant="contained">
+          Create Bucket
+        </Button>
+      </div>
     </form>
   )
 }
--- src/components/fruitForm/fruitForm.component.tsx
+++ src/components/fruitForm/fruitForm.component.tsx
@@ -1,35 +1,116 @@
 import { IFruitFormProps } from '@/types/fruits'
 import { yupResolver } from '@hookform/resolvers/yup'
-import { useForm } from 'react-hook-form'
+import { Button, TextField } from '@mui/material'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 
+export const moneyFormat = (value: string) => {
+  if (!value || value === '') {
+    return ''
+  }
+  let newArrayFormatted = ['R$ ']
+  const numbers = value.replace(/\D/g, '')
+
+  const numbersList = numbers.split('')
+
+  for (let i = 0; i < numbersList.length; i++) {
+    newArrayFormatted.push(numbersList[i])
+  }
+
+  const valueFinally = newArrayFormatted.join('')
+  const result = `${valueFinally}`
+
+  if (numbers.length > 0 && numbers.length < 2) return result.replace('R$ ', '')
+  if (numbers.length === 2) return result.replace(/(.{4})(\d)/, '$1,$2').replace('R$ ', '')
+  if (numbers.length === 3) return result.replace(/(.{4})(\d)(\d)/, '$1,$2$3').replace('R$ ', '')
+  if (numbers.length === 4) return result.replace(/(.{5})(\d)(\d)/, '$1,$2$3').replace('R$ ', '')
+  if (numbers.length === 5) return result.replace(/(.{6})(\d)(\d)/, '$1,$2$3').replace('R$ ', '')
+  if (numbers.length === 6) return result.replace(/(.{4})(\d{3})(\d)/, '$1.$2,$3').replace('R$ ', '')
+  if (numbers.length === 7) return result.replace(/(.{5})(\d{3})(\d)/, '$1.$2,$3').replace('R$ ', '')
+  if (numbers.length === 8) return result.replace(/(.{6})(\d{3})(\d)/, '$1.$2,$3').replace('R$ ', '')
+  if (numbers.length === 9) return result.replace(/(.{4})(\d{3})(\d{3})(\d)/, '$1.$2.$3,$4').replace('R$ ', '')
+  if (numbers.length === 10) return result.replace(/(.{5})(\d{3})(\d{3})(\d)/, '$1.$2.$3,$4').replace('R$ ', '')
+  return numbers.replace('R$ ', '')
+}
+
 const schema = yup.object().shape({
-  name: yup.string().required('Nome é obrigatório'),
-  price: yup.number().required('Preço é obrigatório').positive('Preço deve ser um número positivo'),
+  name: yup.string().required('Name is required'),
+  price: yup.string().required('Price is required'),
 })
 
 export const FruitForm = ({ onSubmit }: IFruitFormProps) => {
+  const [price, setPrice] = useState<string>('')
+
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
+    setValue,
   } = useForm({
     resolver: yupResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      price: '',
+    },
   })
 
+  const handleChangePrice = (price: string) => {
+    const formatPrice = moneyFormat(price)
+    setPrice(formatPrice)
+    setValue('price', formatPrice)
+  }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="name">Nome da Fruta</label>
-        <input id="name" {...register('name')} />
-        {errors.name && <p>{errors.name.message}</p>}
+    <form onSubmit={handleSubmit(onSubmit)} className="max-w-80 flex flex-col gap-y-6">
+      <div className="w-full">
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              placeholder="Type a name"
+              label="Fruit name"
+              error={!!errors.name}
+              helperText={errors.name?.message}
+              variant="outlined"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          )}
+        />
       </div>
-      <div>
-        <label htmlFor="price">Preço da Fruta</label>
-        <input id="price" type="number" step="0.01" {...register('price')} />
-        {errors.price && <p>{errors.price.message}</p>}
+      <div className="w-full">
+        <Controller
+          name="price"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              placeholder="Type a number"
+              label="Price"
+              error={!!errors.price}
+              helperText={errors.price?.message}
+              variant="outlined"
+              fullWidth
+              value={price}
+              onChange={(e) => handleChangePrice(e.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          )}
+        />
       </div>
-      <button type="submit">Adicionar Fruta</button>
+      <div className="flex w-full justify-end -mt-3">
+        <Button type="submit" variant="contained">
+          Add Fruit
+        </Button>
+      </div>
     </form>
   )
 }
