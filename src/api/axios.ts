556db61fc519e661adeff16d22d4f822769cd9d5
Bruna Caletti <c4l3tt1@gmail.com>
2024-06-05 08:57:03 -0300

    Update Axios base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'https://market-fruit-api.vercel.app'
+export const BASE_URL = 'https://market-fruit-api.vercel.app/api'
 //export const BASE_URL = 'http://localhost:3001'
 
 const apiClient = axios.create({
